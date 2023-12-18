273ef0cde9a5541d8f34e51df11608d37ce512d0
amnanaveed074 <127142610+amnanaveed074@users.noreply.github.com>
2023-12-18 16:14:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
+User
 import streamlit as st
 from textblob import TextBlob
 from pattern.en import pluralize, singularize, comparative, superlative
 import streamlit.components.v1 as components
-import nltk
-
-# Download NLTK corpora
-nltk.download('punkt')
 
 def main():
     st.title("Spell Correction")
 
+
+    
     text_data = st.text_area("Enter Text Here")
     a = TextBlob(text_data)
     
     if st.button("Correct"):
         st.success(a.correct())
 
+
+    
     text_data1 = st.text_input("Enter a word For pluralize / singularize")
     
     if st.checkbox("pluralize"):
@@ -24,6 +25,8 @@ def main():
     if st.checkbox("singularize"):
         st.warning(singularize(text_data1))
 
+
+    
     text2 = st.text_input("Enter Text For comparative & superlative")
     
     if st.checkbox("comparative"):
@@ -34,52 +37,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
